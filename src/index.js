d6eedc65d64e662f43a65039dd0e38ddfa649d5b
Stanley <stanleyyylau@gmail.com>
2016-09-26 15:37:26 +0800

    finish setting development envirment

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends Component {
+
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
